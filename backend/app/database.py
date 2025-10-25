1d11ff4ab8a760a0ec24bd38d038a6aeaab36cc8
romainstead <lolkek12042004@gmail.com>
2025-10-25 21:10:51 +0700

    переезд на sqlalchemy + alembic | миграция users в бд (тест)

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,30 +1,43 @@
-from peewee import *
+from datetime import datetime
 import os
 import dotenv
+from sqlalchemy import func
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncAttrs
+from sqlalchemy.orm import DeclarativeBase, declared_attr, Mapped, mapped_column
+from typing import Annotated
 
 dotenv.load_dotenv()
 
-DATABASE = PostgresqlDatabase(
-    os.getenv('DB_NAME'),
-    host=os.getenv('DB_HOST'),
-    port=int(os.getenv('DB_PORT')),
-    user=os.getenv('DB_USERNAME'),
-    password=os.getenv('DB_PASSWORD')
-)
+DB_NAME = os.getenv('DB_NAME')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = int(os.getenv('DB_PORT'))
+DB_USER = os.getenv('DB_USERNAME')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+
+DATABASE_URL = f'postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
+
+# Создание асинхронного подключения к БД
+engine = create_async_engine(DATABASE_URL)
+
+# Создание фабрики сессий
+async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+int_pk = Annotated[int, mapped_column(primary_key=True)]
+created_at = Annotated[datetime, mapped_column(server_default=func.now())]
+updated_at = Annotated[datetime, mapped_column(server_default=func.now(), onupdate=datetime.now)]
+str_uniq = Annotated[str, mapped_column(unique=True, nullable=False)]
+str_null_true = Annotated[str, mapped_column(nullable=True)]
 
 
-class BaseModel(Model):
-    class Meta:
-        database = DATABASE
+class Base(AsyncAttrs, DeclarativeBase):
+    __abstract__ = True
 
+    # Определяет имя таблицы для модели на основе имени класса,
+    # преобразуя его в нижний регистр и добавляя букву 's' в конце.
+    # Например, класс User будет иметь таблицу users.
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return f"{cls.__name__.lower()}s"
 
-def init_db() -> PostgresqlDatabase:
-    """
-    Устанавливает подключение к базе данных, используя данные из .env.
-    :return: Объект PostgresqlDatabase
-    """
-    try:
-        DATABASE.connect()
-    except Exception as e:
-        raise e
-    return DATABASE
+    created_at: Mapped[created_at]
+    updated_at: Mapped[updated_at]
--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,7 +1,9 @@
-from peewee import CharField
-from backend.app.database import BaseModel
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+from backend.app.database import Base, str_uniq, int_pk
 
 
-class User(BaseModel):
-    username = CharField(unique=True)
-    password = CharField()
+class User(Base):
+    id: Mapped[int_pk]
+    login: Mapped[str_uniq] = mapped_column(String(50))
+    password_hash: Mapped[str] = mapped_column(String(255), nullable=False)
